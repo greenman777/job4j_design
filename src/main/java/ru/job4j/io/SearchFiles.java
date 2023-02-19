35920a1129a54df5d82039d22276e870d85a8463
Dmitry Khodyrev <chaos777@mail.ru>
2023-02-19 19:21:36 +0300

    10. Сканирование файловой системы [#106929]

---
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,32 @@
+package ru.job4j.io;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class SearchFiles extends SimpleFileVisitor<Path> {
+    private final Predicate<Path> condition;
+    private final List<Path> fileList = new ArrayList<>();
+
+    public SearchFiles(Predicate<Path> condition) {
+        this.condition = condition;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        if (condition.test(file)) {
+            fileList.add(file);
+        }
+        return CONTINUE;
+    }
+
+    public List<Path> getPaths() {
+        return fileList;
+    }
+}
