d43ebb9226f01324722ce20abb0b5f89051bc5bc
Dmitry Khodyrev <chaos777@mail.ru>
2023-02-19 10:19:09 +0300

    7. Анализ доступности сервера [#859] mod 03

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -6,14 +6,12 @@ public class Analysis {
     public void unavailable(String source, String target) {
         try (BufferedReader read = new BufferedReader(new FileReader(source));
              PrintWriter out = new PrintWriter(target)) {
-            String line, start = null;
-            while ((line = read.readLine()) != null) {
-                String[] split = line.split("\s");
-                if ("400".equals(split[0]) || "500".equals(split[0]) && start == null) {
-                    start = split[1];
-                } else if (start != null) {
-                    out.printf("%s;%s;\n", start, split[1]);
-                    start = null;
+            boolean workTime = true;
+            while (read.ready()) {
+                String[] line = read.readLine().split(" ");
+                if (workTime == Integer.parseInt(line[0]) >= 400) {
+                    out.append(line[1]).append(';').append(workTime ? "" : System.lineSeparator());
+                    workTime = !workTime;
                 }
             }
         } catch (IOException e) {
